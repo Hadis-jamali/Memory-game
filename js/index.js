28984320aa6f27dd52a9c7df5fa4acafc0e987ce
Hadis Jamali <hadisjamali95@gmail.com>
2023-12-11 22:21:42 +0100

    add photo for card and change javascript code and css

--- js/index.js
+++ js/index.js
@@ -1,29 +1,35 @@
 const cardsContainer = document.querySelector(".cards");
 
-const colors = [
-  "pink",
-  "aqua",
-  "crimson",
-  "blue",
-  "gold",
-  "greenyellow",
-  "teal",
-  "aquamarine",
-  "dodgerblue",
+const images = [
+  { name: "sera", img: "./images/front-pic1.png" },
+  { name: "lux", img: "./images/front-pic2.png" },
+  { name: "leblanc", img: "./images/front-pic3.png" },
+  { name: "brand", img: "./images/front-pic4.png" },
+  { name: "morgana", img: "./images/front-pic5.png" },
+  { name: "nami", img: "./images/front-pic6.png" },
+  { name: "leona", img: "./images/front-pic7.png" },
+  { name: "xerath", img: "./images/front-pic8.png" },
 ];
 
-const colorPickList = [...colors, ...colors];
-const color = colorPickList[0];
-
-function buildCard(color) {
+const imagePickList = [...images, ...images];
+const cardCount = imagePickList.length;
+function buildCard(image) {
   const element = document.createElement("div");
   element.classList.add("card");
-  element.setAttribute("background-color", color);
-  element.addEventListener("click", () => {
-    element.style.backgroundColor = color;
+  const img = document.createElement("img");
+  img.classList.add("card-img");
+  img.setAttribute("src", "./images/back-pic.jpg");
+
+  img.addEventListener("click", () => {
+    img.setAttribute("src", image.img);
   });
+  element.appendChild(img);
   return element;
 }
-
-const card = buildCard(color);
-cardsContainer.appendChild(card);
+for (i = 0; i < cardCount; i++) {
+  const randomIndex = Math.floor(Math.random() * imagePickList.length);
+  const image = imagePickList[randomIndex];
+  const card = buildCard(image);
+  imagePickList.splice(randomIndex, 1);
+  cardsContainer.appendChild(card);
+}
