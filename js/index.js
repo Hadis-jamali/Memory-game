7ebf9c979ddc0dadea47ad9cb679f697a4cac33c
Hadis Jamali <hadisjamali95@gmail.com>
2023-12-12 11:14:44 +0100

    select random image from array

--- js/index.js
+++ js/index.js
@@ -1,31 +1,40 @@
 const cardsContainer = document.querySelector(".cards");
 
 const images = [
-  { name: "sera", img: "./images/front-pic1.png" },
-  { name: "lux", img: "./images/front-pic2.png" },
-  { name: "leblanc", img: "./images/front-pic3.png" },
-  { name: "brand", img: "./images/front-pic4.png" },
-  { name: "morgana", img: "./images/front-pic5.png" },
-  { name: "nami", img: "./images/front-pic6.png" },
-  { name: "leona", img: "./images/front-pic7.png" },
-  { name: "xerath", img: "./images/front-pic8.png" },
+  { id: 1, name: "sera", url: "./images/front-pic1.png" },
+  { id: 2, name: "lux", url: "./images/front-pic2.png" },
+  { id: 3, name: "leblanc", url: "./images/front-pic3.png" },
+  { id: 4, name: "brand", url: "./images/front-pic4.png" },
+  { id: 5, name: "morgana", url: "./images/front-pic5.png" },
+  { id: 6, name: "nami", url: "./images/front-pic6.png" },
+  { id: 7, name: "leona", url: "./images/front-pic7.png" },
+  { id: 8, name: "xerath", url: "./images/front-pic8.png" },
 ];
 
 const imagePickList = [...images, ...images];
+
 const cardCount = imagePickList.length;
+
 function buildCard(image) {
   const element = document.createElement("div");
   element.classList.add("card");
+
   const img = document.createElement("img");
   img.classList.add("card-img");
   img.setAttribute("src", "./images/back-pic.jpg");
 
   img.addEventListener("click", () => {
-    img.setAttribute("src", image.img);
+    if (img.getAttribute("src") === image.url) {
+      img.setAttribute("src", "./images/back-pic.jpg");
+    } else {
+      img.setAttribute("src", image.url);
+    }
   });
+
   element.appendChild(img);
   return element;
 }
+
 for (i = 0; i < cardCount; i++) {
   const randomIndex = Math.floor(Math.random() * imagePickList.length);
   const image = imagePickList[randomIndex];
